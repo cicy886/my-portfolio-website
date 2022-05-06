2e9e8028bce39638713319ba4e78c5e358ed6843
Sisi Vieira <svieira@i-ology.com>
2022-05-06 15:39:50 -0700

    Add icons to Nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {BsBriefcase} from 'react-icons/bs'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><BsBriefcase/></a>
+      <a href="#contract"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
