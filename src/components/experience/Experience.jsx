749f950294f27108967b9896cf09585065cb0d93
Sisi Vieira <svieira@i-ology.com>
2022-05-19 17:47:17 -0700

    Update content and styling for experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return (
-    <section id='experience'>experience</section>
-  )
-}
+    return (
+        <section id="experience">
+            <h5>Skills I Have</h5>
+            <h2>My Experience</h2>
 
-export default Experience
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>HTML</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>CSS</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>SASS</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>JavaScript</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>React</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>Vue</h4>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="experience__backend">
+                    <h3>Backend Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>C#/.Net</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>SQL</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>noSQL</h4>
+                            </div>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon"/>
+                            <div>
+                                <h4>Node.js</h4>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Experience;
