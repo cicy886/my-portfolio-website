73906bc1712c9505dd0a9236f101bd0225619445
Sisi Vieira <svieira@i-ology.com>
2022-05-31 17:49:32 -0700

    Add content, styling and package for contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,103 @@
-import React from 'react'
-import './contact.css'
+import React, { useRef } from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return (
-    <section id='contact'>contact</section>
-  )
-}
+    const form = useRef();
 
-export default Contact
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_vn963q9",
+                "template_lpi1upx",
+                form.current,
+                "zQVrfH-_NtGKWvcAb"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+
+        e.target.reset();
+    };
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineEmail className="contact__option-icon" />
+                        <h4>Email</h4>
+                        <h5>cicy886@gmail.com</h5>
+                        <a
+                            href="mailto:cicy886@gmail.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Send a message
+                        </a>
+                    </article>
+                    <article className="contact__option">
+                        <RiMessengerLine className="contact__option-icon" />
+                        <h4>Messenger</h4>
+                        <a
+                            href="https://m.me/cicy.mo"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Send a message
+                        </a>
+                    </article>
+                    <article className="contact__option">
+                        <BsWhatsapp className="contact__option-icon" />
+                        <h4>WhatsApp</h4>
+                        <a
+                            href="https://wa.me/+15034198085"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Send a message
+                        </a>
+                    </article>
+                </div>
+                {/* END OF CONTACT OPTIONS */}
+                <form ref={form} onSubmit={sendEmail}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your Email Address"
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        rows="7"
+                        placeholder="Your Message"
+                        required
+                    ></textarea>
+                    <button type="submite" className="btn btn-primary">
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
